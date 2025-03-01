966c6184416dc2172b6a5c99343a96658a3a7565
Phoenix <159592948+Muffinzo@users.noreply.github.com>
2025-03-01 01:44:24 +0100

    Update main.lua

--- muffin_logs/client/main.lua
+++ muffin_logs/client/main.lua
@@ -1,7 +1,7 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
-    if config.debug == True then
+    if config.debug == true then
         print("✅ ", "Heartbeat from playerload has been accepted")
     end
     local playerId = source
@@ -9,7 +9,7 @@ AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
 
    
     TriggerServerEvent("muffin_logs:logJoin", playerIden)
-    if config.debug == True then
+    if config.debug == true then
         print(" ✅ ", "Trigger server event")
     end
 end)
